43ed7800c48d9a318405dcfb4758490e839dce78
Tsu-gu <108401269+Tsu-gu@users.noreply.github.com>
2024-09-26 19:48:31 +0200

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Create a desktop entry
+wget https://raw.githubusercontent.com/Tsu-gu/flashpoint-ubuntu-install/main/fp.png
+mv fp.png $HOME/Pictures/
+wget https://raw.githubusercontent.com/Tsu-gu/flashpoint-ubuntu-install/main/flashpoint.desktop
+awk -v home="$HOME" '{gsub("/home/tsugu", home); print}' flashpoint.desktop > new_flashpoint.desktop
+cp new_flashpoint.desktop $HOME/.local/share/applications/flashpoint.desktop
+chmod +x $HOME/.local/share/applications/flashpoint.desktop
+rm new_flashpoint.desktop
+rm flashpoint.desktop
+read -p "Press Enter to close...."
