fa9a1139a29977c153906776c3c30c09941c3623
Tsu-gu <108401269+Tsu-gu@users.noreply.github.com>
2024-09-27 17:10:20 +0200

    Add files via upload

--- flashpoint-ubuntu-installer.sh
+++ flashpoint-ubuntu-installer.sh
@@ -32,8 +32,9 @@ mv fp.png $HOME/Pictures/
 wget https://raw.githubusercontent.com/Tsu-gu/flashpoint-ubuntu-install/main/flashpoint.desktop
 awk -v home="$HOME" '{gsub("/home/tsugu", home); print}' flashpoint.desktop > new_flashpoint.desktop
 cp new_flashpoint.desktop $HOME/.local/share/applications/flashpoint.desktop
-chmod +x $HOME/.local/share/applications/flashpoint.desktop
 chmod 0444 $HOME/.local/share/applications/flashpoint.desktop
+chmod +x $HOME/.local/share/applications/flashpoint.desktop
+gio set $HOME/.local/share/applications/flashpoint.desktop metadata::trusted true
 rm new_flashpoint.desktop
 rm flashpoint.desktop
 read -p "Press Enter to close...."
