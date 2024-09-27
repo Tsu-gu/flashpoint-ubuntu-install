c4a381a2b8795005c8ca63f21247ac7b4e57465e
Tsu-gu <108401269+Tsu-gu@users.noreply.github.com>
2024-09-27 22:49:32 +0200

    Create flashpoint-debian-installer.sh

---
+++ flashpoint-debian-installer.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# discovers which ubuntu you are running
+debianversion=$(cat /etc/os-release | grep "VERSION_CODENAME=" | sed 's/VERSION_CODENAME=//' | sed 's/"//g')
+
+# Dependencies
+sudo dpkg --add-architecture i386
+sudo apt install libxcomposite1:i386 -y
+sudo apt install pulseaudio xserver-xorg-core libgtk-3-0 libnss3 php p7zip -y
+
+# Install Wine from their repo
+sudo mkdir -pm755 /etc/apt/keyrings
+sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/debian/dists/$debianversion/winehq-$debianversion.sources
+sudo apt update
+sleep 1
+
+# Wine does not package stable releases for 24.04 yet.
+if [ "$debianversion" == "trixie" ]; then
+    sudo apt install --install-recommends winehq-devel -y
+else
+    sudo apt install --install-recommends winehq-stable -y
+fi
+
+# Download and unzip flashpoint's file
+filename="fp13_linux_20240425m.7z" 
+wget https://download.unstable.life/upload/$filename
+mkdir $HOME/Flashpoint/
+mv $filename $HOME/Flashpoint/
+cd $HOME/Flashpoint/
+7zr x fp13_linux_*.7z -oFlashpoint
+sleep 1
+rm $filename
+
+cd $HOME/Desktop/
+# Create a desktop entry
+wget https://raw.githubusercontent.com/Tsu-gu/flashpoint-ubuntu-install/main/fp.png
+mv fp.png $HOME/Pictures/
+wget https://raw.githubusercontent.com/Tsu-gu/flashpoint-ubuntu-install/main/1_flashpoint.desktop
+awk -v home="$HOME" '{gsub("/home/tsugu", home); print}' 1_flashpoint.desktop > flashpoint.desktop
+gio set $HOME/Desktop/flashpoint.desktop metadata::trusted true
+chmod +x flashpoint.desktop
+rm 1_flashpoint.desktop
+read -p "Press Enter to close...."
+
+
